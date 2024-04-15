520232f5ec71c405b56ae22b00cd4c8a11cc793e
Jeff Hall <jeff.leon.hall@gmail.com>
2024-04-15 16:58:43 -0700

    add answer to exercise

--- src/main/scala/Chapter3/Chapter3Exercises.scala
+++ src/main/scala/Chapter3/Chapter3Exercises.scala
@@ -9,5 +9,38 @@ import doodle.java2d._
 import cats.effect.unsafe.implicits.global
 
 object Chapter3Exercises extends App {
-  
+
+  // 3.1.1
+
+  val c1 = Image.circle(1)
+  val c2 = Image.circle(10)
+  val c3 = Image.circle(100)
+
+  c1.draw()
+  c2.draw()
+  c3.draw()
+
+  // 3.1.2
+
+  Image.circle(1)
+  Image.rectangle(10, 100)
+  Image.triangle(200, 50)
+
+  // The type of circle is: Image
+  // The type of rectangle is: Image
+  // The type of triangle is: Image
+
+  // 3.1.3
+
+  // Image.circle(10).draw()
+  // The type of drawing an image is Unit.
+  // This means that the draw method does not return any useful value.
+
+  // 3.1.4
+
+  Image.circle(40)
+    .beside(Image.circle(40))
+    .beside(Image.circle(40))
+    .on(Image.circle(120))
+    .draw()
 }
