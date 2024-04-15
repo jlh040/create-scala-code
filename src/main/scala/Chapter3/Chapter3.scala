03949bbdaec3b1d324aff80ec59d9114b7335270
Jeff Hall <jeff.leon.hall@gmail.com>
2024-04-15 15:52:34 -0700

    add gitignore to the project

---
+++ src/main/scala/Chapter3/Chapter3.scala
@@ -0,0 +1,9 @@
+import doodle.core._
+import doodle.image._
+import doodle.image.syntax._
+import doodle.image.syntax.core._
+import doodle.java2d._
+
+object Chapter3 extends App {
+  println(1)
+}
